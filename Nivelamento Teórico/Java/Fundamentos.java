a014f8a73a931788556e8b112d91c53281b2806e
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-11-29 19:45:10 -0300

    :fire:

---
+++ Nivelamento Teórico/Java/Fundamentos.java
@@ -0,0 +1,18 @@
+// Hello Word
+// @author João C. Prestes
+public class Fundamentos {
+    public static void main(String[] args) {
+        String nome;
+        int idade = 18;
+        char sexo = 'M'; // Para char sempre utilziar aspas simples
+        double temperatura = 25.00;
+        boolean arCondicinado = false;
+        nome = "João Claudio prestes";
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Temperatura: " + temperatura);
+        System.out.println("Ar condicionado: " + arCondicinado);
+    }
+}
+
--- Nivelamento Teórico/Java/Hello.java
+++ Nivelamento Teórico/Java/Hello.java
@@ -1,8 +1,8 @@
 // Hello Word
 // @author João C. Prestes
-
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
     }
 }
